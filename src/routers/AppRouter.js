c80e76fc43d3fc0d2383c3c37702096fb91f329d
Todd Langford-Archer <toddpla@gmail.com>
2018-12-06 13:34:16 +0000

    AppRouter added
    This will allow for the user to be redirected to the GamePage after 
    completing the setup.

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import SetupPage from '../components/SetupPage';
+import NotFoundPage from '../components/NotFoundPage';
+// import Header from '../components/Header';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div className="container-fluid">
+      // <Header />
+      <Switch>
+        <Route path="/" component={SetupPage} exact={true}/>
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+)
+
+export default AppRouter;
