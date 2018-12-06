883fe6e7298415fda23482bf1cbc5a41151633ff
Todd Langford-Archer <toddpla@gmail.com>
2018-12-06 11:44:27 +0000

    ships seed data
    added seed data for the ships to be added to users on init

---
+++ src/seeds/ships.js
@@ -0,0 +1,21 @@
+export default [{
+  id: 1,
+  name: 'Carrier',
+  size: 5
+}, {
+  id: 2,
+  name: 'Battleship',
+  size: 4
+}, {
+  id: 3,
+  name: 'Cruiser',
+  size: 3
+}, {
+  id: 4,
+  name: 'Submarine',
+  size: 3
+}, {
+  id: 5,
+  name: 'Destroyer',
+  size: 2
+}]
