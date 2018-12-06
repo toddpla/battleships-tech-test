5822e379003bf99e3a06f575a0d8bb382330b73d
Todd Langford-Archer <toddpla@gmail.com>
2018-12-06 10:12:20 +0000

    added START_GAME action
    START_GAME action takes two parameters:
      - player_one
      - players_two
    
    The reducer then needs to assign the default state for the players

---
+++ src/actions/GameActions.js
@@ -0,0 +1,5 @@
+export const startGame = (player_one, player_two) => ({
+  TYPE: 'START_GAME',
+  player_one,
+  player_two
+})
---
+++ src/tests/fixtures/players.js
@@ -0,0 +1,7 @@
+export default [{
+  name: 'Sophie',
+  ships: []
+}, {
+  name: 'Frankie',
+  ships: []
+}]
