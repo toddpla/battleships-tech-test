c2016f55619bc3150909162d58e5f9d891f7108c
Todd Langford-Archer <toddpla@gmail.com>
2018-12-06 11:45:39 +0000

    Game Action TYPE changed to type

--- src/actions/GameActions.js
+++ src/actions/GameActions.js
@@ -1,5 +1,13 @@
 export const startGame = (player_one, player_two) => ({
-  TYPE: 'START_GAME',
+  type: 'START_GAME',
   player_one,
   player_two
 })
+
+// export const placeShip = (player, x, y, z) => ({
+//   TYPE: 'PLACE_SHIP',
+//   player,
+//   x,
+//   y,
+//   z
+// })
