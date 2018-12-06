3c78775fef8509a38abfea8c31a3f74e49240869
Todd Langford-Archer <toddpla@gmail.com>
2018-12-06 11:48:39 +0000

    added Game reducer with START_GAME
    This reducer action will init a game adding the two players and 
    allocating the ships from the seed data.
    
    Redux also added including the following packages:
      -redux
      -redux-react
      -redux-thunk

---
+++ src/reducers/GameReducer.js
@@ -0,0 +1,13 @@
+import ships from '../seeds/ships'
+
+const gameReducerDefaultState = {};
+
+export default (state = gameReducerDefaultState, action) => {
+  switch (action.type) {
+    case 'START_GAME':
+      return {
+        player_one: {...action.player_one, ships},
+        player_two: {...action.player_two, ships}
+      }
+  }
+}
---
+++ src/tests/reducers/GameReducers.test.js
@@ -0,0 +1,17 @@
+import gameReducer from '../../reducers/GameReducer'
+import players from '../fixtures/players'
+
+test('should set default state', () => {
+  const state = gameReducer(undefined, {type: '@@INIT'})
+})
+
+test('should add players to game', () => {
+  let player_one =  players[0]
+  let player_two = players[1]
+  const action = {
+    type: 'START_GAME',
+    player_one,
+    player_two
+  }
+  const state = gameReducer(player_one, player_two)
+})
